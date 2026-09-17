df43ea3857a85098b072d3e8dace4aa93fe0a68c
jovanSlavujevic96 <jotzilla96@gmail.com>
2026-09-17 21:10:42 +0200

    Hello world with React using classes

---
+++ frontend/my-app/src/AppClass.js
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+export default class AppClass extends Component {
+
+    // mandatory method for every class which extends `Component`
+    render() {
+        return (
+            <h1>Hello, World!</h1>
+        );
+    }
+}
