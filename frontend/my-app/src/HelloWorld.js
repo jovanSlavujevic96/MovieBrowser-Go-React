9e3901a876e708289c8aecb0418cbad8d985b21e
jovanSlavujevic96 <jotzilla96@gmail.com>
2026-09-17 21:46:31 +0200

    using props: passing data to components

--- frontend/my-app/src/HelloWorld.js
+++ frontend/my-app/src/HelloWorld.js
@@ -1,12 +1,12 @@
 import React, { Fragment } from 'react';
 import './HelloWorld.css';
 
-function HelloWorld() {
+function HelloWorld(props) {
     return (
         // <hr/> - horizontal rule
         <Fragment>
             <hr/>
-            <h1 className='h1-green'>Hello, World!</h1>
+            <h1 className='h1-green'>{props.msg}</h1>
         </Fragment>
     )
 }
