93d4c84425c6ae38d570a7f66997a2f8f689e1f9
jovanSlavujevic96 <jotzilla96@gmail.com>
2026-09-17 22:24:30 +0200

    React & State 1

--- frontend/my-app/src/HelloWorld.js
+++ frontend/my-app/src/HelloWorld.js
@@ -1,12 +1,29 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import './HelloWorld.css';
 
 function HelloWorld(props) {
+    const [isTrue, setIsTrue] = useState(false)
+
     return (
         // <hr/> - horizontal rule
         <Fragment>
             <hr/>
             <h1 className='h1-green'>{props.msg}</h1>
+            <hr/>
+            {
+                isTrue ? 
+                    <p>Is true</p> :
+                    <p>Is false</p>
+            }
+            {
+                isTrue && 
+                <Fragment>
+                        <hr/>
+                        <p>The current value of isTrue is true</p>
+                    </Fragment>
+            }
+            <hr/>
+            <button htef="#!" className="btn btn-outline-secondary" onClick={ () => { setIsTrue(!isTrue) }}>Toggle isTrue</button>
         </Fragment>
     )
 }
