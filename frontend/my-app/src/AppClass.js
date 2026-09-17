4cf9cb7f425ea8232daadc8365adeed2293fd24d
jovanSlavujevic96 <jotzilla96@gmail.com>
2026-09-17 21:21:27 +0200

    Styling components

--- frontend/my-app/src/AppClass.js
+++ frontend/my-app/src/AppClass.js
@@ -1,10 +1,11 @@
 import { Component } from 'react';
+import './AppClass.css'
 
 export default class AppClass extends Component {
     // mandatory method for every class which extends `Component`
     render() {
         return (
-            <h1>Hello, World!</h1>
+            <h1 className="h1-red">Hello, World!</h1>
         );
     }
 }
